28fa9c184a7b03cfa7d063b4efa4bf3407b81740
Salomatov Nikita <togglenezt@yandex.ru>
2023-02-17 17:06:08 +0300

    fix: cors запросы 2

--- frontend/src/utils/utils.js
+++
@@ -1,5 +0,0 @@
-//Объявление переменных
-const serverUrl = 'http://api.mesto-api.nomoredomains.work'; // Адрес сервера
-const serverToken = '44789594-7e2d-4206-b282-20acc9a01a01';//токен авторизации
-
-export { serverUrl, serverToken };
