a70c9cf20cdf58249ab196fbd43e635830b4cb1d
Salomatov Nikita <togglenezt@yandex.ru>
2023-02-20 16:33:07 +0300

    fix: KEY 8

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,19 +1,18 @@
 const jwt = require('jsonwebtoken');
 const AuthErr = require('../errors/AuthErr');
 
-const { NODE_ENV, JWT_SECRET ="eee" } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
-      throw new AuthErr('Необходима авторизация');
+    throw new AuthErr('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret');
   } catch (err) {
     return next(new AuthErr('Необходима авторизация'));
   }
