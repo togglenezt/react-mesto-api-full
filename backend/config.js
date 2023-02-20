1bc742a5954dbc3a9e040939af82079f39710d2c
Salomatov Nikita <togglenezt@yandex.ru>
2023-02-20 16:38:10 +0300

    fix: KEY 12

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET = 'JWT_SECRET' } = process.env;
+const { JWT_SECRET = 'dev' } = process.env;
 const { MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 module.exports = {
   JWT_SECRET,
