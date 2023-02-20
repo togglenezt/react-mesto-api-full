72caea12c9ab090bd58f0baa133fdbb895af7246
Salomatov Nikita <togglenezt@yandex.ru>
2023-02-20 15:25:57 +0300

    fix: key

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -42,7 +42,7 @@ module.exports.login = (req, res, next) => {
   return userSchema
     .findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key', {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret', {
         expiresIn: '7d',
       });
       res.status(200).send({ token });
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
   } catch (err) {
     return next(new AuthErr('Необходима авторизация'));
   }
