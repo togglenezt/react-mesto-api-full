33eb8056bc9acf10298c5c951eafcdd0df27c558
Salomatov Nikita <togglenezt@yandex.ru>
2023-02-20 16:28:52 +0300

    fix: KEY 7

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,13 +6,11 @@ const { NODE_ENV, JWT_SECRET ="eee" } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  const JWT = req.cookies.jwt;
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    if(!JWT) {
       throw new AuthErr('Необходима авторизация');
-    }
   }
-  const token = !authorization ? JWT : authorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
